8b63522a3ad90690f28c9c6c40eb2b32b8ab8cba
wiwi7750 <63353279+wiwi7750@users.noreply.github.com>
2023-10-05 16:01:16 +0800

    Add files via upload

---
+++ qa_chat.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  2 09:48:32 2023
+
+@author: william.chen
+"""
+
+# imports
+import ast  # for converting embeddings saved as strings back to arrays
+import openai  # for calling the OpenAI API
+import pandas as pd  # for storing text and embeddings data
+import tiktoken  # for counting tokens
+from scipy import spatial  # for calculating vector similarities for search
+
+
+# models
+EMBEDDING_MODEL = "text-embedding-ada-002"
+GPT_MODEL = "gpt-3.5-turbo"
+openai.api_key = "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"
+
+
+# df = pd.read_csv("AWS FAQ CSV.csv",header=None)
+# df.columns=["text"]
+
+df = pd.read_pickle('ecv_billing_qa.pk')
+
+def get_embedding(text):
+  response = openai.Embedding.create(
+    model=EMBEDDING_MODEL,  # Make sure EMBEDDING_MODEL is defined somewhere
+    input=text
+  )
+  return response['data'][0]['embedding']  # Assuming the embedding is in the 'data' field
+
+# df['embedding'] = df['text'].apply(get_embedding)
+# df.head()
+# df.to_pickle("ecv_billing_qa.pk")
+
+# search function
+def strings_ranked_by_relatedness(
+    query: str,
+    df: pd.DataFrame,
+    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+    top_n: int = 100
+) -> tuple[list[str], list[float]]:
+    """Returns a list of strings and relatednesses, sorted from most related to least."""
+    query_embedding_response = openai.Embedding.create(
+        model=EMBEDDING_MODEL,
+        input=query,
+    )
+    query_embedding = query_embedding_response["data"][0]["embedding"]
+    strings_and_relatednesses = [
+        (row["text"], relatedness_fn(query_embedding, row["embedding"]))
+        for i, row in df.iterrows()
+    ]
+    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+    strings, relatednesses = zip(*strings_and_relatednesses)
+    return strings[:top_n], relatednesses[:top_n]
+
+
+def num_tokens(text: str, model: str = GPT_MODEL) -> int:
+    """Return the number of tokens in a string."""
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(text))
+
+
+def query_message(
+    query: str,
+    df: pd.DataFrame,
+    model: str,
+    token_budget: int
+) -> str:
+    """Return a message for GPT, with relevant source texts pulled from a dataframe."""
+    strings, relatednesses = strings_ranked_by_relatedness(query, df)
+    introduction = 'Use the below eCloudvalley billing team FAQ doc to answer the subsequent question. If the answer cannot be found in the articles, write "I could not find an answer."'
+    question = f"\n\nQuestion: {query}"
+    message = introduction
+    for string in strings:
+        next_article = f'\n\nQ&A:\n"""\n{string}\n"""'
+        if (
+            num_tokens(message + next_article + question, model=model)
+            > token_budget
+        ):
+            break
+        else:
+            message += next_article
+    return message + question
+
+
+def ask(
+    query: str,
+    df: pd.DataFrame = df,
+    model: str = GPT_MODEL,
+    token_budget: int = 4096 - 500,
+    print_message: bool = False,
+) -> str:
+    """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
+    message = query_message(query, df, model=model, token_budget=token_budget)
+    if print_message:
+        print(message)
+    messages = [
+        {"role": "system", "content": "You answer questions about the billing team questions, try to rephrase into a better answer and format as bullet form."},
+        {"role": "user", "content": message},
+    ]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    response_message = response["choices"][0]["message"]["content"]
+    return response_message
+
+
+answer = ask('whats the use of credit from AWS?')
+print(answer)
